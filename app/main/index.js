94c0d8fc9817f0e6da3186bdf37e2b9e4b4d8b11
GellyBird <zombiecupcake0409@gmail.com>
2026-04-12 23:04:58 -0400

    base header created, minor css

---
+++ app/main/index.js
@@ -0,0 +1,13 @@
+let logged = false;
+
+const accountOpts = document.getElementById("accountOptions")
+
+
+function hideElement(element) {
+    element.style.display = "none"
+}
+
+function showElement(element) {
+    element.style.display = "flex"
+}
+
